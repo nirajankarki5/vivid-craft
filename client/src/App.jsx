d704c5ddb943608717fcf9f261e032a4f62e40f2
nirajankarki5 <nirajankarki5@gmail.com>
2024-05-28 12:54:16 -0400

    added client folder setup

---
+++ client/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>VIVID CRAFT APP.jsx</h1>;
+}
+
+export default App;
--- script.js
+++
@@ -1,6 +0,0 @@
-const headilgEl = document.querySelector(".heading");
-
-headilgEl.addEventListener(
-  "mouseover",
-  (e) => (headilgEl.style.color = "#ff0000")
-);
