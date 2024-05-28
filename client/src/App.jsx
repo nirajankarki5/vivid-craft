b234d6e5937ce6773c7aac4fd1c9b14ad071e979
nirajankarki5 <nirajankarki5@gmail.com>
2024-05-28 13:17:53 -0400

    tailwind css setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 function App() {
-  return <h1>VIVID CRAFT APP.jsx</h1>;
+  return (
+    <h1 className="text-blue-500 text-4xl font-bold text-center">VIVID CRAFT APP.jsx</h1>
+  );
 }
 
 export default App;
