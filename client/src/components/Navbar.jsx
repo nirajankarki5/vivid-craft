257dd7d7f771cef4cbf9afd40af41c0ea2356229
nirajankarki5 <nirajankarki5@gmail.com>
2024-05-31 12:49:07 -0400

    navbar design improvements

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,13 @@ const Navbar = () => {
 
   return (
     <nav className="relative flex items-center justify-between gap-6 border-b-gray-100 px-8 py-4 md:gap-10 lg:gap-14">
-      <img className="h-16" src={logo} alt="vivid craft logo" />
+      <Link
+        to={"/"}
+        className="flex items-center gap-2 font-serif text-2xl font-semibold text-[#555]"
+      >
+        <img className="h-10" src={logo} alt="vivid craft logo" />
+        <p>Vivid Craft</p>
+      </Link>
 
       <FaBars
         onClick={() => setIsNavlinkShown(!isNavlinkShown)}
@@ -16,18 +22,18 @@ const Navbar = () => {
       />
 
       <form
-        className={`${isNavlinkShown ? "absolute left-0 top-24 flex h-14" : "hidden"} w-full items-center gap-2 bg-gray-100 px-4 sm:flex sm:h-10 sm:rounded-full lg:h-12 lg:w-[50rem]`}
+        className={`${isNavlinkShown ? "absolute left-0 top-16 flex h-14" : "hidden"} w-full items-center gap-4 bg-white px-4 sm:flex sm:h-10 lg:h-12 lg:w-[50rem]`}
       >
-        <FaMagnifyingGlass />
+        <FaMagnifyingGlass className="text-gray-400" />
         <input
-          className="h-full w-full bg-gray-100 focus:outline-none"
+          className="h-full w-full focus:outline-none"
           type="text"
           placeholder="Search image..."
         />
       </form>
 
       <div
-        className={`${isNavlinkShown ? "absolute left-0 top-[9.5rem] flex w-full flex-col items-center py-6" : "hidden"} gap-6 bg-white sm:flex md:gap-10 lg:gap-14`}
+        className={`${isNavlinkShown ? "absolute left-0 top-[7.5rem] flex w-full flex-col items-center py-6" : "hidden"} gap-6 bg-white sm:flex md:gap-10 lg:gap-14`}
       >
         <Link to="/about">About</Link>
         <Link to="/login">Log&nbsp;in</Link>
