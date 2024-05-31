993b6dbe74bc05ffa8a3eba24abe2fea52f55816
nirajankarki5 <nirajankarki5@gmail.com>
2024-05-31 10:59:33 -0400

    added navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
-import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FaMagnifyingGlass, FaBars } from "react-icons/fa6";
 
 const Navbar = () => {
+  const [isNavlinkShown, setIsNavlinkShown] = useState(false);
+
   return (
-    <nav className="flex justify-between  px-6 py-4">
+    <nav className="relative flex items-center justify-between gap-6 border-b-gray-100 px-8 py-4 md:gap-10 lg:gap-14">
       <img className="h-16" src={logo} alt="vivid craft logo" />
 
-      <form className="flex h-10 items-center gap-2 rounded-full bg-gray-100 px-4">
+      <FaBars
+        onClick={() => setIsNavlinkShown(!isNavlinkShown)}
+        className="cursor-pointer text-2xl sm:hidden"
+      />
+
+      <form
+        className={`${isNavlinkShown ? "absolute left-0 top-24 flex" : "hidden"} h-10 w-full items-center gap-2 rounded-full bg-gray-100 px-4 sm:flex lg:h-12 lg:w-[50rem]`}
+      >
         <FaMagnifyingGlass />
         <input
-          className="h-full bg-gray-100"
+          className="h-full w-full bg-gray-100 focus:outline-none"
           type="text"
           placeholder="Search image..."
         />
       </form>
 
-      <div className="flex gap-4">
+      <div
+        className={`${isNavlinkShown ? "absolute top-40 flex" : "hidden"} gap-6 sm:flex md:gap-10 lg:gap-14`}
+      >
         <Link to="/about">About</Link>
-        <Link to="/login">Log in</Link>
-        <Link to="/signup">Sign up</Link>
+        <Link to="/login">Log&nbsp;in</Link>
+        <Link to="/signup">Sign&nbsp;up</Link>
       </div>
     </nav>
   );
