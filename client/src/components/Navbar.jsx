4da64dc4e5166b4e93fc0b996520a21f0f5c7b2e
nirajankarki5 <nirajankarki5@gmail.com>
2024-06-10 10:09:14 -0400

    added scrollable navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
 import { FaMagnifyingGlass, FaBars } from "react-icons/fa6";
 
 const Navbar = () => {
   const [isNavlinkShown, setIsNavlinkShown] = useState(false);
+  const [prevScrollpos, setPrevScrollpos] = useState(window.pageYOffset);
+  const [top, setTop] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        setTop(0);
+      } else {
+        setTop(-85);
+      }
+      setPrevScrollpos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollpos]);
 
   return (
-    <nav className="relative flex items-center justify-between gap-6 border-b-gray-100 px-8 py-4 md:gap-10 lg:gap-14">
+    <nav
+      style={{ top: `${top}px` }}
+      className="transition-top fixed top-0 flex h-20 w-full items-center justify-between gap-6 border-b-gray-100 bg-white px-8 duration-300 md:gap-10 lg:gap-14"
+    >
       <Link
         to={"/"}
         className="flex items-center gap-2 font-serif text-2xl font-semibold text-[#555]"
       >
         <img className="h-10" src={logo} alt="vivid craft logo" />
-        <p>Vivid Craft</p>
+        <p className="w-36 leading-6">Vivid Craft</p>
       </Link>
 
       <FaBars
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <h1 className="h-screen bg-blue-500">Home</h1>;
+  return <h1 className="mt-[5rem] h-screen bg-blue-500 sm:mt-20">Home</h1>;
 };
 
 export default Home;
