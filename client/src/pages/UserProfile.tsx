f8544bb322a0bf38135db62144d8eec8236067d8
nirajankarki5 <nirajankarki5@gmail.com>
2024-06-29 15:10:32 -0400

    user profile design updates

--- client/src/pages/UserProfile.tsx
+++ client/src/pages/UserProfile.tsx
@@ -15,7 +15,7 @@ const UserProfile = () => {
   return (
     <>
       <div className="mx-auto grid justify-center gap-1 md:w-[45rem] md:grid-cols-2 md:justify-center">
-        <aside className="mb-4 mt-8 flex h-36 items-center justify-center md:h-44 lg:h-56">
+        <aside className="mb-1 mt-4 flex h-36 items-center justify-center md:mb-4 md:mt-8 md:h-44 lg:h-56">
           <div className="flex h-full w-36 items-center justify-center rounded-full bg-gray-200 md:w-44 lg:w-56">
             <FaRegUser className="mx-auto text-5xl text-gray-400" />
             {/* <img src="" alt="" /> */}
@@ -28,7 +28,7 @@ const UserProfile = () => {
           </p>
           <p>email@gmail.com</p>
 
-          <p className="w-80 pt-5 text-center text-sm text-gray-400 md:w-auto md:text-left md:text-base">
+          <p className="w-80 pt-2 text-center text-sm text-gray-400 md:w-auto md:pt-5 md:text-left md:text-base">
             Get gorgeous, high-quality pictures that User has handpicked for
             free.
           </p>
