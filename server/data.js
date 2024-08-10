25a6aea2d30a7e681818194be27985e4e9eebdf9
nirajankarki5 <nirajankarki5@gmail.com>
2024-08-10 14:13:35 -0400

    upadted data.js

--- server/data.js
+++ server/data.js
@@ -4,132 +4,132 @@ module.exports = [
     imageUrl:
       "https://images.unsplash.com/photo-1690575539214-eb0ade6cdd4d?q=80&w=1854&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     category: "Nature",
-    tags: ["nature", "rocks", "photography"],
+    tags: ["nature", "rocks", "photography", "ocean"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://images.unsplash.com/photo-1713769918995-9d1a7607d051?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     category: "Nature",
-    tags: ["nature", "rocks", "photography"],
+    tags: ["nature", "mountain", "forest", "river"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://images.unsplash.com/photo-1708261432863-41fdab7b5de9?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     category: "Nature",
-    tags: ["nature", "rocks", "photography"],
+    tags: ["nature", "flower", "photography"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://plus.unsplash.com/premium_photo-1664391605349-92b7ec8385e2?q=80&w=1882&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    category: "Illustration",
-    tags: ["nature", "rocks", "photography"],
+    category: "Painting",
+    tags: ["animal", "deer", "sketch", "painting"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://images.unsplash.com/photo-1579762715118-a6f1d4b934f1?q=80&w=1884&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     category: "Illustration",
-    tags: ["nature", "rocks", "photography"],
+    tags: ["birds", "art"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://images.unsplash.com/photo-1580130732455-6663b4ce9f6e?q=80&w=1956&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     category: "Illustration",
-    tags: ["nature", "rocks", "photography"],
+    tags: ["painting", "atr", "poster"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    category: "Illustration",
-    tags: ["nature", "rocks", "photography"],
+    category: "Food & Drink",
+    tags: ["food", "healthy", "photography", "salad", "breakfast", "food bowl"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?q=80&w=1910&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    category: "Illustration",
-    tags: ["nature", "rocks", "photography"],
+    category: "Food & Drink",
+    tags: ["food", "eggs", "healthy", "salad"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://plus.unsplash.com/premium_photo-1711136314731-8c3fe1831672?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    category: "Illustration",
-    tags: ["nature", "rocks", "photography"],
+    category: "Nature",
+    tags: ["nature", "scenery", "tree", "moon", "lake"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://images.unsplash.com/photo-1634986666676-ec8fd927c23d?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    category: "Illustration",
-    tags: ["nature", "rocks", "photography"],
+    category: "Painting",
+    tags: ["painting", "statue", "photography"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://images.unsplash.com/photo-1593073862407-a3ce22748763?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     category: "Illustration",
-    tags: ["nature", "rocks", "photography"],
+    tags: ["people", "lights", "photography"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://images.unsplash.com/photo-1435575653489-b0873ec954e2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    category: "Illustration",
-    tags: ["nature", "rocks", "photography"],
+    category: "Architecture & Interiors",
+    tags: ["building", "glass", "tall-buildings"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://images.unsplash.com/photo-1492321936769-b49830bc1d1e?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     category: "Illustration",
-    tags: ["nature", "rocks", "photography"],
+    tags: ["triangle", "pattern", "photography"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://images.unsplash.com/photo-1644522911882-4ebb0f3bb79d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    category: "Illustration",
-    tags: ["nature", "rocks", "photography"],
+    category: "Architecture & Interiors",
+    tags: ["buildings", "glass", "skyscraper", "sky", "apartment"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://plus.unsplash.com/premium_photo-1717628520104-43a4e0cc6c7b?q=80&w=1943&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    category: "Illustration",
-    tags: ["nature", "rocks", "photography"],
+    category: "Painting",
+    tags: ["painting", "colorful", "photography"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://images.unsplash.com/photo-1718046881184-d2e3ac15f236?q=80&w=1828&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    category: "Illustration",
-    tags: ["nature", "rocks", "photography"],
+    category: "Fashion & Beauty",
+    tags: ["fashion", "girl", "beauty", "pose"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://images.unsplash.com/photo-1655219282830-7ecd31afac68?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    category: "Illustration",
-    tags: ["nature", "rocks", "photography"],
+    category: "Nature",
+    tags: ["nature", "fox", "red-fox", "animal"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://images.unsplash.com/photo-1592873289208-a5de05694ea1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    category: "Illustration",
-    tags: ["nature", "rocks", "photography"],
+    category: "Architecture & Interiors",
+    tags: ["interior", "decor", "photography"],
   },
   {
     userId: "6611a0beba36e0dadaf564ef",
     imageUrl:
       "https://images.unsplash.com/photo-1623607037050-ac8e7cce86c6?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D2",
-    category: "Illustration",
-    tags: ["nature", "rocks", "photography"],
+    category: "Fashion & Beauty",
+    tags: ["parfum", "fashion", "beauty"],
   },
 ];
