c3b22427423b89d4d9b93a5580aa727f7993f41c
nirajankarki5 <nirajankarki5@gmail.com>
2024-08-17 17:39:01 -0400

    added about page

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,5 +1,30 @@
 const About: React.FC = () => {
-  return <>About</>;
+  return (
+    <div className="px-4 sm:px-8 md:text-lg">
+      <h1 className="py-8 text-4xl">Vivid Craft</h1>
+      <p className="lg:mr-40 ">
+        Vivid Craft is a software company that aims to fuse creative art and
+        architecture with precise code to provide a digital user experience that
+        goes beyond simple usability, transforming experiences into digital art.
+      </p>
+      <br />
+      <p>
+        Their application features a rich <strong>IMAGE GALLERY</strong> where
+        users can:
+        <ul className="ml-10 list-disc">
+          <li> Upload images</li>
+          <li> View uploaded images</li>
+          <li> Add favorite images to a personal collection</li>
+          <li> Download images for personal use</li>
+        </ul>
+        <br />
+        The images showcased are all open source and free to use. They can be
+        displayed by category, and users can add tags for improved search
+        functionality. Additionally, users can see the number of "likes"
+        (favorites) for each image.
+      </p>
+    </div>
+  );
 };
 
 export default About;
