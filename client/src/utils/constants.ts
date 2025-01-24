5265884fdcb8bade070fd7941990de53d7005a3a
nirajankarki5 <nirajankarki5@gmail.com>
2025-01-24 18:15:18 -0500

    baseurl change

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,5 +1,5 @@
 // export const baseUrl: string = "http://localhost:5000/api";
-// export const baseUrl: string = "https://vivid-craft.onrender.com/api";
+export const baseUrl: string = "https://vivid-craft.onrender.com/api";
 
 // EC2 instance IP address
-export const baseUrl: string = "http://54.86.163.19:5000/api";
+// export const baseUrl: string = "https://54.86.163.19:5000/api";
